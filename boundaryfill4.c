368d40a1c9b82e8926074fb118b2b827904dad92
VaishnaviR <163028415+vaish-navi26@users.noreply.github.com>
2024-05-01 14:43:21 +0530

    Add files via upload

---
+++ boundaryfill4.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+
+
+void poly(){
+    rectangle(100,100,140,140);
+}
+
+void boundfill(int x,int y,int fc,int bc){
+if(getpixel(x,y)!=fc && getpixel(x,y)!=bc){
+   putpixel(x,y,fc);
+   bf(x+1,y,fc,bc);
+   bf(x-1,y,fc,bc);
+   bf(x,y+1,fc,bc);
+   bf(x,y-1,fc,bc);
+}
+}
+int main(){
+int gd=DETECT,gm,c;
+
+initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
+
+        poly();
+        boundfill(105,105,13,15);
+        getch();
+       closegraph();
+
+return 0;
+}
+
