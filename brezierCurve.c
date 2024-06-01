be2d9878730bd113a5020002b0337f41186bb9dd
Vaishnavi Ramani <163028415+vaish-navi26@users.noreply.github.com>
2024-06-01 16:12:11 +0530

    Update brezierCurve.c

--- brezierCurve.c
+++ brezierCurve.c
@@ -1,52 +1,33 @@
-# include <stdio.h>
-# include <conio.h>
-# include <graphics.h>
-# include <math.h>
+#include <stdio.h>
+#include <graphics.h>
+#include <math.h>
 
-double ncr(int n, int r) {
-    double result = 1;
-    int i;
-    for ( i = 1; i <= r; i++) { 
-        result *= (double)(n - i + 1) / i;
+
+void drawBezierCurve(int x[], int y[]) {
+    float t;
+    setcolor(BLUE);
+
+  
+    for (t = 0.0; t <= 1.0; t += 0.001) {
+        float xt, yt;
+        xt = pow(1 - t, 3) * x[0] + 3 * t * pow(1 - t, 2) * x[1] + 3 * pow(t, 2) * (1 - t) * x[2] + pow(t, 3) * x[3];
+        yt = pow(1 - t, 3) * y[0] + 3 * t * pow(1 - t, 2) * y[1] + 3 * pow(t, 2) * (1 - t) * y[2] + pow(t, 3) * y[3];
+        putpixel(xt, yt, RED);
     }
-    return result;
 }
 
-int main(){
+int main() {
+    int gd = DETECT, gm;
+    int choice;
+    int bezierX[] = {100, 200, 300, 400};
+    int bezierY[] = {100, 300, 200, 100};
 
-    int points;
-    int x[points],y[points],i = 0,j;
-    double puty = 0,putx = 0,t;
-    int gd=DETECT,gm;
-    printf("Enter the number of points for Curve: ");
-    scanf("%d", &points);
+    initgraph(&gd, &gm, "C://TURBOC3//BGI");
 
-    initgraph(&gd,&gm,(char*)"");
-    for ( i = 0; i < points; i++)
-    {
-        printf("Enter x and y coordinated of point %d: ",i+1);
-        scanf("%d%d",&x[i],&y[i]);
-        
-    }
+    drawBezierCurve(bezierX, bezierY);
 
-    for( i = 0; i < points - 1; i++){
-        line(x[i],y[i],x[i + 1], y[i + 1]);
-    }
-
-    for(t = 0.0; t <= 1.0; t += 0.001){
-
-    putx = 0;
-    puty = 0;
-    for( j = 0; j < points; j++){
-        
-        putx += ncr(points - 1, j) * pow(1 - t, points - 1 - j) * pow(t, j) * x[j];
-        puty += ncr(points - 1, j) * pow(1 - t, points - 1 - j) * pow(t, j) * y[j];
-    }
-    putpixel((int)putx, (int)puty, WHITE); 
+closegraph();
+return 0;
 }
 
 
-    getch();
-    closegraph();
-    return 0;
-}
