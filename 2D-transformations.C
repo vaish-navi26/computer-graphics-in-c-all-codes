b903c9269f28ebf08431117a4ce5e58c1acb6653
Vaishnavi Ramani <163028415+vaish-navi26@users.noreply.github.com>
2024-06-01 21:05:41 +0530

    Update 2D-transformations.C

--- 2D-transformations.C
+++ 2D-transformations.C
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<conio.h>
 #include<graphics.h>
+#include<math.h>
 #define r 4
 
 float T[3][3] = {{1,0,0},
@@ -19,6 +20,9 @@ float R[3][3] =  {{1,0,0},
 		  {0,1,0},
 		  {0,0,1}};
 
+float RO[3][3] = {{1,0,0},
+		  {0,1,0},
+		  {0,0,1}};
 
 float A[r][3] = {{20,20,1},
 	       {100,20,1},
@@ -26,7 +30,43 @@ float A[r][3] = {{20,20,1},
 	       {20,100,1}};
 float mul[r][3];
 
-/* void reflection(float rx, float ry)
+void rotation(float angle,int dir)
+{
+ int i,j,k;
+ float x,y,a;
+ a = angle * M_PI /180.0;
+ x = cos(a);
+ y = sin(a);
+ if (dir == 1)
+ {
+  RO[0][0] = x;
+  RO[0][1] = -y;
+  RO[1][0] = y;
+  RO[1][1] = x;
+ }
+ else if (dir == 2)
+ {
+  RO[0][0] = x;
+  RO[0][1] = y;
+  RO[1][0] = -y;
+  RO[1][1] = x;
+ }
+
+
+ for (i=0;i<r;i++)
+ {
+  for (j=0;j<3;j++)
+  {
+   mul[i][j]=0;
+   for (k=0;k<3;k++)
+   {
+    mul[i][j] += A[i][k] * RO[k][j];
+   }
+  }
+ }
+}
+
+void reflection(float rx, float ry)
 {
  int i,j,k;
  R[0][0] = rx;
@@ -44,7 +84,7 @@ float mul[r][3];
   }
  }
 }
-*/
+
 void shear(float shx, float shy)
 {
  int i,j,k;
@@ -103,6 +143,33 @@ void translate(float tx, float ty)
 
 }
 
+void rotation_point(float angle, int dir)
+{
+ int i,j;
+ translate(-20.0,-20.0);
+
+ for (i=0;i<r;i++)
+ {
+  for (j=0;j<3;j++)
+  {
+   A[i][j] = mul[i][j];
+  }
+ }
+
+ rotation(angle,dir);
+
+  for (i=0;i<r;i++)
+ {
+  for (j=0;j<3;j++)
+  {
+   A[i][j] = mul[i][j];
+  }
+ }
+
+ translate(20.0,20.0);
+
+}
+
 
 
 
@@ -112,10 +179,11 @@ void main()
  float tx = 50,ty = 50;
  float sx = 0.5,sy = 0.5;
  float shx = 1,shy = 1;
+ float angle = 30;
  int choi;
 
  initgraph(&gd,&gm,"C:\\TURBOC3\\BGI");
- printf("\n1). Translation \n2). Scaling \n3). Reflection on y-axis \n4). Reflection on x-axis \n5). Reflection about origin \n6). Shear-x \n7). Shear-y");
+ printf("\n1). Translation \n2). Scaling \n3). Reflection on y-axis \n4). Reflection on x-axis \n5). Reflection about origin \n6). Shear-x \n7). Shear-y \n8). Rotation(clock) \n9). Rotation(anti-clock) \n10). Rotation about point(clock) \n11). Rotation about point(anti-clock)");
  printf("\nEnter choi: ");
  scanf("%d",&choi);
  cleardevice();
@@ -136,7 +204,7 @@ void main()
   case 2:
     scale(sx,sy);
     break;
-/*
+
   case 3:
     reflection(-1,1);
     break;
@@ -148,13 +216,29 @@ void main()
   case 5:
     reflection(-1,-1);
     break;
-*/
+
   case 6:
     shear(shx,0);
     break;
 
   case 7:
     shear(0,shy);
+    break;
+
+  case 8:
+    rotation(angle,1);
+    break;
+
+  case 9:
+    rotation(angle,2);
+    break;
+
+  case 10:
+    rotation_point(angle,1);
+    break;
+
+  case 11:
+    rotation_point(angle,2);
     break;
 
   default:
